d1b8c1c7b5735627df4e8f9fff163b51bccb1285
hyunjin <alfma123@naver.com>
2022-10-01 14:44:07 +0900

    :sparkles: use bindActionCreators for dispatch
    use bindActionCreators for dispatch

--- src/containers/CounterContainer.jsx
+++ src/containers/CounterContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
 import Counter from '../components/Counter';
@@ -13,12 +14,12 @@ export default connect(
   (state) => ({
     number: state.counter.number,
   }),
-  (dispatch) => ({
-    increase: () => {
-      dispatch(increase());
-    },
-    decrease: () => {
-      dispatch(decrease());
-    },
-  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        increase,
+        decrease,
+      },
+      dispatch,
+    ),
 )(CounterContainer);
